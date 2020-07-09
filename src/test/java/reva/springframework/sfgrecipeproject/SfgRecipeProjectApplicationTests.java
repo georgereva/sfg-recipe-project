a92bc7829ce1335c2a6ddbc1d5f41fa2c8a1ccbc
georgereva <joravartan@gmail.com>
2020-07-09 19:18:43 +0300

    add new project

---
+++ src/test/java/reva/springframework/sfgrecipeproject/SfgRecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package reva.springframework.sfgrecipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
