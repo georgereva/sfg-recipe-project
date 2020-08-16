4b127e9239ab9462b6813ff8f67f780d79b12003
georgereva <joravartan@gmail.com>
2020-08-16 22:25:20 +0300

    updating with Project Lombok

--- src/main/java/reva/springframework/sfgrecipeproject/controllers/IndexController.java
+++ src/main/java/reva/springframework/sfgrecipeproject/controllers/IndexController.java
@@ -1,11 +1,13 @@
 package reva.springframework.sfgrecipeproject.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import reva.springframework.sfgrecipeproject.services.RecipeService;
 
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -17,9 +19,9 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
+        log.debug("I'm in the controller");
 
         model.addAttribute("recipes", recipeService.getRecipes());
-
         return "index";
     }
 }
--- src/main/java/reva/springframework/sfgrecipeproject/services/RecipeServiceImpl.java
+++ src/main/java/reva/springframework/sfgrecipeproject/services/RecipeServiceImpl.java
@@ -1,5 +1,6 @@
 package reva.springframework.sfgrecipeproject.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reva.springframework.sfgrecipeproject.domain.Recipe;
 import reva.springframework.sfgrecipeproject.repositories.RecipeRepository;
@@ -7,6 +8,7 @@ import reva.springframework.sfgrecipeproject.repositories.RecipeRepository;
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
+
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
